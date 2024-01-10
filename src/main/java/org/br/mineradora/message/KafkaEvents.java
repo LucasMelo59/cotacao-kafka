d48cedf4135d3ad27dc33e9577b195f6484a78d0
JOAO LUCAS MELO <lucasmelo59@unifor.br>
2024-01-10 13:28:10 -0300

    adicionando dependencia do kafka e classe de emissão de eventos

---
+++ src/main/java/org/br/mineradora/message/KafkaEvents.java
@@ -0,0 +1,25 @@
+package org.br.mineradora.message;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import org.br.mineradora.dto.QuotationDTO;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@ApplicationScoped //responsável por enviar informações das cotações para um tópico kakfa
+public class KafkaEvents {
+
+    private final Logger LOG = LoggerFactory.getLogger(KafkaEvents.class);
+
+
+    @Channel("quotation-channel") //canal de acesso ao tópico do kakfa
+    Emitter<QuotationDTO> quotationRequestEmitter; // Mensável e tipo da mensagem que será enviada para o kafka
+
+    public void sendNewKafkEvent(QuotationDTO quotation) {
+        LOG.info("-- Enviando Cotação para Tópico Kafka --");
+        quotationRequestEmitter.send(quotation).toCompletableFuture().join();
+    }
+
+}
